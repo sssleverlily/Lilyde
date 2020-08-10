182b48ef3599c56035f2fe5c30f3388574adbe44
sssleverlily <13094266681@163.com>
2020-08-10 22:21:44 +0800

    fix:改一下配置

--- app/src/main/java/com/example/lilyde/lilyde/Test/MainActivity.kt
+++ app/src/main/java/com/example/lilyde/lilyde/Test/MainActivity.kt
@@ -11,14 +11,17 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import coil.Coil
+import coil.ImageLoader
 import coil.api.load
 import coil.transform.CircleCropTransformation
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import com.example.lilyde.Lilyde
 import com.example.lilyde.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -51,7 +54,7 @@ class MainActivity : AppCompatActivity(){
     var url2 = "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=579539400,2248223712&fm=26&gp=0.jpg"
     var url3 = "http://img.mp.itc.cn/upload/20170311/48180d37e4474628900d058f3cc5ee7d_th.gif"
     var url4 = "http://img.mp.itc.cn/upload/20170311/33f2b7f7ffb04ecb81e42405e20b3fdc_th.gif"
-    var url5 = "/Users/ssslever/Desktop/timg.jpeg"
+    var url5 = "https://user-gold-cdn.xitu.io/2020/7/15/17351bf4013357d3?imageslim.jpg"
     var url6 = "/Users/ssslever/Desktop/Lilyde/app/src/main/res/drawable/trees_on_mountain.jpg"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,27 +66,27 @@ class MainActivity : AppCompatActivity(){
     private fun initView() {
 //        FrescoTest()
 //        glideTest()
-        lilydeTest()
+//        lilydeTest()
 //        picassoTest()
-        loadFromMemory()
+//        loadFromMemory()
 
         //注解
-        Glide.with(this)
-                .`as`(FrameSequenceDrawable::class.java)
-                .load(url3)
-                .into(iv_3)
+//        Glide.with(this)
+//                .`as`(FrameSequenceDrawable::class.java)
+//                .load(url3)
+//                .into(iv_3)
         //circleProgressView.visibility = View.VISIBLE
 
-//        iv_0.loadImage(this, url3,onProgressListener = object : OnProgressListener {
-//            override fun onProgress(isComplete: Boolean, percentage: Int, bytesRead: Long, totalBytes: Long) {
-//                // 跟踪进度
-//                if (isComplete) {
-//                    //网络太快看不见进度条，先设置看见
-//                    circleProgressView.visibility = View.VISIBLE
-//                }
-//                circleProgressView.progress = percentage
-//            }
-//        })
+        iv_0.loadImage(this, url3,onProgressListener = object : OnProgressListener {
+            override fun onProgress(isComplete: Boolean, percentage: Int, bytesRead: Long, totalBytes: Long) {
+                // 跟踪进度
+                if (isComplete) {
+                    //网络太快看不见进度条，先设置看见
+                    circleProgressView.visibility = View.GONE
+                }
+                circleProgressView.progress = percentage
+            }
+        })
 
 //        val animator = ValueAnimator.ofInt(0, 100)
 //        animator.duration = 2000
@@ -91,39 +94,59 @@ class MainActivity : AppCompatActivity(){
 //        animator.interpolator = LinearInterpolator()
 //        animator.addUpdateListener { circleProgressView.progress = (animator.animatedValue as Int) }
 //        animator.start()
-        Lilyde.placeHolderImageView = R.color.red
-        Lilyde.circlePlaceholderImageView = R.color.red
-//        iv_1.setOnClickListener { downloadImage() }
-//        iv_1.loadImage(this, url3)
-//        iv_2.loadImage(this, url4, requestListener = object : RequestListener<Drawable?> {
-//            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable?>?, isFirstResource: Boolean): Boolean {
-//                Toast.makeText(application, R.string.load_failed, Toast.LENGTH_LONG).show()
-//                return false
-//            }
-//
-//            override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable?>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-//                Toast.makeText(application, R.string.load_success, Toast.LENGTH_LONG).show()
-//                return false
-//            }
-//        })
+//        Lilyde.placeHolderImageView = R.color.red
+//        Lilyde.circlePlaceholderImageView = R.color.red
+        iv_17.load(url5){
+            crossfade(6000)
+        }
+        iv_1.setOnClickListener { downloadImage() }
+        iv_1.loadImage(this, url3)
+        iv_2.loadImage(this, url4, requestListener = object : RequestListener<Drawable?> {
+            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable?>?, isFirstResource: Boolean): Boolean {
+                Toast.makeText(application, R.string.load_failed, Toast.LENGTH_LONG).show()
+                return false
+            }
 
-//        iv_3.loadBlurImage(this, url4)
-//        iv_4.loadCircleImage(this, url4)
-//        iv_5.loadRoundCornerImage(this, url4)
-//        iv_6.loadGrayImage(this, url4)
-//        iv_7.loadResizeXYImage(this, url2, 800, 200)
-//        iv_8.loadImageWithTransformation(this, url2, GrayscaleTransformation(), RoundedCornersTransformation(50, 0))
-//        iv_9.loadCircleWithBorderImage(this, url2)
-//        iv_10.loadImageWithTransformation(this, url2, BlurTransformation(this, 20), GrayscaleTransformation(), CircleCrop())
-//        iv_11.loadImage(this, R.drawable.ic_launcher_background)
-//        iv_12.loadImage(this, "")
-//        iv_13.loadBorderImage(this, url2)
+            override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable?>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                Toast.makeText(application, R.string.load_success, Toast.LENGTH_LONG).show()
+                return false
+            }
+        })
+        val drawableCrossFadeFactory =
+                 DrawableCrossFadeFactory.Builder(6000).setCrossFadeEnabled(true).build()
+
+        Glide.with(this)
+                .load(url1)
+                .transition(DrawableTransitionOptions.with(drawableCrossFadeFactory))
+                .into(iv_3)
+        //iv_3.loadBlurImage(this, url4)
+        iv_4.loadCircleImage(this, url4)
+        iv_5.loadRoundCornerImage(this, url4)
+        iv_6.loadGrayImage(this, url4)
+        iv_7.loadResizeXYImage(this, url2, 800, 200)
+        iv_8.loadImageWithTransformation(this, url2, GrayscaleTransformation(), RoundedCornersTransformation(50, 0))
+        iv_9.loadCircleWithBorderImage(this, url2)
+        iv_10.loadImageWithTransformation(this, url2, BlurTransformation(this, 20), GrayscaleTransformation(), CircleCrop())
+        iv_11.loadImage(this, R.drawable.ic_launcher_background)
+        iv_12.loadImage(this, "")
+        iv_13.loadBorderImage(this, url2)
 
     }
 
     private fun FrescoTest(){
         val startTime = Date().time
-        val simpleDrawView= findViewById(R.id.iv_14) as? SimpleDraweeView
+        val simpleDrawView1= findViewById(R.id.iv_0) as? SimpleDraweeView
+        val simpleDrawView2= findViewById(R.id.iv_1) as? SimpleDraweeView
+        val simpleDrawView3= findViewById(R.id.iv_2) as? SimpleDraweeView
+        val simpleDrawView4= findViewById(R.id.iv_3) as? SimpleDraweeView
+        val simpleDrawView5= findViewById(R.id.iv_4) as? SimpleDraweeView
+        val simpleDrawView6= findViewById(R.id.iv_5) as? SimpleDraweeView
+        val simpleDrawView7= findViewById(R.id.iv_6) as? SimpleDraweeView
+        val simpleDrawView8= findViewById(R.id.iv_7) as? SimpleDraweeView
+        val simpleDrawView9= findViewById(R.id.iv_8) as? SimpleDraweeView
+        val simpleDrawView10= findViewById(R.id.iv_9) as? SimpleDraweeView
+        val simpleDrawView11= findViewById(R.id.iv_10) as? SimpleDraweeView
+
 //        simpleDrawView?.setImageURI(url3)  非动图
 
         val controller = Fresco.newDraweeControllerBuilder()
@@ -131,41 +154,108 @@ class MainActivity : AppCompatActivity(){
                 .setAutoPlayAnimations(true)//设置是否自动播放
                 .build()
 
-        simpleDrawView?.controller = controller  //动图为什么动不了？
-        val endTime = Date().time
-        val totalHeapSize = Runtime.getRuntime().totalMemory()
-        val freeHeapSize = Runtime.getRuntime().freeMemory()
-        val heapSize = totalHeapSize - freeHeapSize
-        Log.e("lilyz_fresco",(endTime - startTime).toString()+"  java_heap:"+heapSize/10000f)
+        simpleDrawView1?.controller = controller  //动图为什么动不了？
+        simpleDrawView2?.controller = controller
+        simpleDrawView3?.controller = controller
+        simpleDrawView4?.controller = controller
+        simpleDrawView5?.controller = controller
+        simpleDrawView6?.controller = controller
+        simpleDrawView7?.controller = controller
+        simpleDrawView8?.controller = controller
+        simpleDrawView9?.controller = controller
+        simpleDrawView10?.controller = controller
+        simpleDrawView11?.controller = controller
+//        val endTime = Date().time
+//        val totalHeapSize = Runtime.getRuntime().totalMemory()
+//        val freeHeapSize = Runtime.getRuntime().freeMemory()
+//        val heapSize = totalHeapSize - freeHeapSize
+//        Log.e("lilyz_fresco",(endTime - startTime).toString()+"  java_heap:"+heapSize/10000f)
     }
 
     private fun glideTest(){
-        val startTime = Date().time
-        Glide.with(this)
-                .load(url3)
-                .into(iv_1)
-        val endTime = Date().time
-        val totalHeapSize = Runtime.getRuntime().totalMemory()
-        val freeHeapSize = Runtime.getRuntime().freeMemory()
-        val heapSize = totalHeapSize - freeHeapSize
-        Log.e("lilyz_glide",(endTime - startTime).toString()+"  java_heap:"+heapSize/10000f)
+//        val startTime = Date().time
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_1)
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_2)
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_3)
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_4)
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_5)
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_6)
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_7)
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_8)
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_9)
+//        Glide.with(this)
+//                .load(url3)
+//                .into(iv_10)
+//        val endTime = Date().time
+//        val totalHeapSize = Runtime.getRuntime().totalMemory()
+//        val freeHeapSize = Runtime.getRuntime().freeMemory()
+//        val heapSize = totalHeapSize - freeHeapSize
+//        Log.e("lilyz_glide",(endTime - startTime).toString()+"  java_heap:"+heapSize/10000f)
     }
 
     private fun picassoTest(){
         val startTime = Date().time
         Picasso.with(this)
                 .load(url3)
+                .into(iv_0)
+        Picasso.with(this)
+                .load(url3)
+                .into(iv_1)
+        Picasso.with(this)
+                .load(url3)
                 .into(iv_2)
-        val endTime = Date().time
-        val totalHeapSize = Runtime.getRuntime().totalMemory()
-        val freeHeapSize = Runtime.getRuntime().freeMemory()
-        val heapSize = totalHeapSize - freeHeapSize
-        Log.e("lilyz_picasso",(endTime - startTime).toString()+"  java_heap:"+heapSize/10000f)
+        Picasso.with(this)
+                .load(url3)
+                .into(iv_3)
+        Picasso.with(this)
+                .load(url3)
+                .into(iv_4)
+        Picasso.with(this)
+                .load(url3)
+                .into(iv_5)
+        Picasso.with(this)
+                .load(url3)
+                .into(iv_6)
+        Picasso.with(this)
+                .load(url3)
+                .into(iv_7)
+        Picasso.with(this)
+                .load(url3)
+                .into(iv_8)
+        Picasso.with(this)
+                .load(url3)
+                .into(iv_9)
+        Picasso.with(this)
+                .load(url3)
+                .into(iv_10)
+//        val endTime = Date().time
+//        val totalHeapSize = Runtime.getRuntime().totalMemory()
+//        val freeHeapSize = Runtime.getRuntime().freeMemory()
+//        val heapSize = totalHeapSize - freeHeapSize
+//        Log.e("lilyz_picasso",(endTime - startTime).toString()+"  java_heap:"+heapSize/10000f)
 
     }
 
     private fun lilydeTest(){
-        circleProgressView.visibility = View.VISIBLE
+//        circleProgressView.visibility = View.VISIBLE
         val startTime = Date().time
 //        iv_0.loadImage(this, url1,onProgressListener = object : OnProgressListener {
 //            override fun onProgress(isComplete: Boolean, percentage: Int, bytesRead: Long, totalBytes: Long) {
@@ -177,14 +267,25 @@ class MainActivity : AppCompatActivity(){
 //                circleProgressView.progress = percentage
 //            }
 //        })
-        iv_0.loadImage(this,url1)
-        val endTime = Date().time
-        val totalHeapSize = Runtime.getRuntime().totalMemory()
-        val freeHeapSize = Runtime.getRuntime().freeMemory()
-        val heapSize = totalHeapSize - freeHeapSize
-        val activityManager = this.getSystemService(Context.ACTIVITY_SERVICE)
+        iv_0.loadImage(this,url3)
+        iv_1.loadImage(this,url3)
+        iv_2.loadImage(this,url3)
+        iv_3.loadImage(this,url3)
+        iv_4.loadImage(this,url3)
+        iv_5.loadImage(this,url3)
+        iv_6.loadImage(this,url3)
+        iv_7.loadImage(this,url3)
+        iv_8.loadImage(this,url3)
+        iv_9.loadImage(this,url3)
+        iv_10.loadImage(this,url3)
 
-        Log.e("lilyz_lilyde",(endTime - startTime).toString()+"  java_heap:"+heapSize/10000f)
+//        val endTime = Date().time
+//        val totalHeapSize = Runtime.getRuntime().totalMemory()
+//        val freeHeapSize = Runtime.getRuntime().freeMemory()
+//        val heapSize = totalHeapSize - freeHeapSize
+//        val activityManager = this.getSystemService(Context.ACTIVITY_SERVICE)
+//
+//        Log.e("lilyz_lilyde",(endTime - startTime).toString()+"  java_heap:"+heapSize/10000f)
     }
 
     private fun hasStoragePermission(): Boolean {
@@ -211,9 +312,16 @@ class MainActivity : AppCompatActivity(){
 
     //想看一下从内存加载是什么情况
     fun loadFromMemory(){
-        iv_1.load(url1){
-            crossfade(true)
+        iv_3.load(url5){
+            crossfade(6000)
         }
+
+        val imageLoader = ImageLoader.Builder(this)
+                .availableMemoryPercentage(0.25)
+                .crossfade(6000)
+                .build()
+
+        iv_1
 
 
     }
